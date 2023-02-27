e7cfecc16d537b0055d70303aa631446bc947ecf
Franklin Obiukwu <obiukwuchibuisi@gmail.com>
2023-02-27 12:02:48 +0100

    basic code structure

--- js/liss.js
+++
@@ -1,2 +0,0 @@
-Abigail are you there? 
-
--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
- 
+//Update Loop
+import Ball from './ball.js'
+
+const ball = new Ball(document.getElementById('ball'))
+
+function update(time) {
+  console.log(time)
+  window.requestAnimationFrame(update)
+}
+
+window.requestAnimationFrame(update)
